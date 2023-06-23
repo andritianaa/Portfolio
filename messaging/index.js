74e7a12d7d250b66383295c2b74fc7939e06f326
andritianaa <andritianasteve@gmail.com>
2023-06-23 23:23:41 +0300

    remove cors

--- messaging/index.js
+++ messaging/index.js
@@ -19,7 +19,7 @@ const app = express()
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-app.use(cors({ origin: 'https://andritiana.tech' }))
+app.use(cors())
 
 app.post('/', async (req, res) => {
     console.log(req.body)
